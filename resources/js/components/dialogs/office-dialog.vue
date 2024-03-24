95c2c8c77ee5ae14fac34850ddaf34f8a2c82456
sairlseb <cyrilseberiaga@gmail.com>
2024-03-24 10:29:14 +0800

    added a create office function

--- resources/js/components/dialogs/office-dialog.vue
+++ resources/js/components/dialogs/office-dialog.vue
@@ -4,21 +4,25 @@
         max-width="500"
     >
         <v-card class="pa-5">
-            <v-card-title>Offices</v-card-title>
-            <v-card-text>
-                <v-text-field label="Enter Office Name"></v-text-field>
-            </v-card-text>
-            <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="secondary" @click="closeDialog">Close</v-btn>
-                <v-btn color="primary">Add Office</v-btn>
-            </v-card-actions>
+            <v-form @submit.prevent="addOffice">
+                <v-card-title>Offices</v-card-title>
+                <v-card-text>
+                    <v-text-field label="Enter Office Name" v-model="office_name"></v-text-field>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="secondary" @click="closeDialog">Close</v-btn>
+                    <v-btn color="primary" type="submit">Add Office</v-btn>
+                </v-card-actions>
+            </v-form>
+           
         </v-card>
     </v-dialog>
 </template>
 
 <script>
-import { watch } from 'vue'
+import { ref, watch } from 'vue'
+import axios from '@axios'
 export default {
     props: {
         visible: {
@@ -28,7 +32,7 @@ export default {
     },
     setup(props, { emit }) {
         const visible = ref(false)
-
+        const office_name = ref(null)
 
         watch(
             () => props.visible,
@@ -37,16 +41,37 @@ export default {
             }
         )
 
+        const addOffice = () => {
+            axios.post('/office', {
+                name: office_name.value
+            })
+            .then(({data}) => {
+                console.log(data)
+                closeDialog()
+            })
+            .catch((error) => {
+                console.log(error.response.data)
+            })
+            
+        }
+
         const closeDialog = () => {
             emit('close')
+            clearInputs()
         }
+
+        const clearInputs = () => {
+            office_name.value = null
+        }   
 
         return {
             visible,
+            office_name,
             
 
             //methods
             closeDialog,
+            addOffice,
         }
     },
 }
