095a56f90a891528e7d78e73520b5e9a0ab709ff
sairlseb <cyrilseberiaga@gmail.com>
2024-03-24 10:02:00 +0800

    added office page and routes

---
+++ resources/js/views/pages/offices/offices.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col>
+                <v-card class="pa-5">
+                    <v-card-title class="d-flex justify-space-between">
+                        <h4>Offices</h4>
+                        <v-btn color="primary">Add Office</v-btn>
+                    </v-card-title>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
