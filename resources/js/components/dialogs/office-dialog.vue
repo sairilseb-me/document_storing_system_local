f9b033c11e3e41bececc88408e3bfb845a6ac5be
sairlseb <cyrilseberiaga@gmail.com>
2024-03-24 10:12:29 +0800

    added dialog for office

---
+++ resources/js/components/dialogs/office-dialog.vue
@@ -0,0 +1,53 @@
+<template>
+    <v-dialog
+        v-model="visible"
+        max-width="500"
+    >
+        <v-card class="pa-5">
+            <v-card-title>Offices</v-card-title>
+            <v-card-text>
+                <v-text-field label="Enter Office Name"></v-text-field>
+            </v-card-text>
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="secondary" @click="closeDialog">Close</v-btn>
+                <v-btn color="primary">Add Office</v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+import { watch } from 'vue'
+export default {
+    props: {
+        visible: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    setup(props, { emit }) {
+        const visible = ref(false)
+
+
+        watch(
+            () => props.visible,
+            (value) => {
+                visible.value = value
+            }
+        )
+
+        const closeDialog = () => {
+            emit('close')
+        }
+
+        return {
+            visible,
+            
+
+            //methods
+            closeDialog,
+        }
+    },
+}
+</script>
--- resources/js/views/pages/offices/offices.vue
+++ resources/js/views/pages/offices/offices.vue
@@ -4,11 +4,38 @@
             <v-col>
                 <v-card class="pa-5">
                     <v-card-title class="d-flex justify-space-between">
-                        <h4>Offices</h4>
-                        <v-btn color="primary">Add Office</v-btn>
+                        <h4 class="text-h5">Offices</h4>
+                        <v-btn color="primary" @click="showOfficeDialog = true">Add Office</v-btn>
                     </v-card-title>
                 </v-card>
             </v-col>
         </v-row>
+        <office-dialog :visible="showOfficeDialog" @close="closeShowOfficeDialog"></office-dialog>
     </v-container>
 </template>
+
+<script>
+
+import OfficeDialog from '@/components/dialogs/office-dialog.vue'
+export default {
+    components: {
+        OfficeDialog
+    },
+    setup() {
+        const showOfficeDialog = ref(false)
+
+        const closeShowOfficeDialog = () => {
+            showOfficeDialog.value = false
+        }
+
+        return {
+            //variable
+            showOfficeDialog,
+
+
+            //methods
+            closeShowOfficeDialog
+        }
+    },
+}
+</script>
