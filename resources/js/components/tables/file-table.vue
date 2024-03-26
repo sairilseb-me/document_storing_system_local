b8e651b27083c3f63c8aa9bf36c014857a6d7abc
sairlseb <cyrilseberiaga@gmail.com>
2024-03-26 16:10:13 +0800

    added a table for files

---
+++ resources/js/components/tables/file-table.vue
@@ -0,0 +1,52 @@
+<template>
+    <v-card class="pa-5">
+        <v-card-text>
+            <v-data-table
+                :headers="headers"
+            >
+                <template #[`item.actions`] = "{item}">
+                    <v-icon color="secondary">mdi-view</v-icon>
+                    <v-icon color="warning">mdi-pencil</v-icon>
+                    <v-icon color="error">mdi-trash</v-icon>
+                </template>
+            </v-data-table>
+        </v-card-text>
+    </v-card>
+    
+</template>
+
+<script>
+export default {
+    setup() {
+        const headers = ref([
+            {
+                title: 'Filename',
+                key: 'filename'
+            },
+            {
+                title: 'Office From',
+                key: 'office_from'
+            },
+            {
+                title: 'Route From',
+                key: 'route_from'
+            },
+            {
+                title: 'Date Received',
+                key: 'date_received'
+            },
+            {
+                title: 'Actions',
+                key: 'actions'
+            }
+        ])
+
+
+        return {
+            //variables
+            headers,
+
+        }
+    },
+}
+</script>
--- resources/js/views/pages/files/files.vue
+++ resources/js/views/pages/files/files.vue
@@ -3,17 +3,32 @@
         <v-row>
             <v-col>
                 <v-card class="pa-5">
-                    <v-catd-title class="d-flex justify-space-between">
+                    <v-card-title class="d-flex justify-space-between">
                         <h4 class="text-h5">Files</h4>
                         <v-btn color="primary">Add File</v-btn>
-                    </v-catd-title>
+                    </v-card-title>
                 </v-card>
             </v-col>
         </v-row>
         <v-row>
             <v-col>
-                
+                <file-table></file-table>
             </v-col>
         </v-row>
     </v-container>
 </template>
+
+<script>
+
+import { ref } from 'vue'
+import axios from '@axios'
+import FileTable from '@/components/tables/file-table.vue'
+export default {
+    components: {
+        FileTable
+    },
+    setup() {
+        
+    },
+}
+</script>
