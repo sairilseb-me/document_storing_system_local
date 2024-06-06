50844dd0e9fc3dfc98e449629ffd37c802ac30a3
Cyril Seberiaga <cyrilseberiaga@gmail.com>
2024-06-06 15:20:47 +0800

    update on re routing

--- resources/js/plugins/axios/index.js
+++ resources/js/plugins/axios/index.js
@@ -1,7 +1,47 @@
 import axios from 'axios'
+import { useGlobalSnackbarStore } from '@/store/GlobalSnackbar'
+
+
 
 const axiosIns = axios.create({
-    baseURL: 'http://localhost:8000/api'
+    baseURL: 'http://localhost:8000/api',
+    headers: {
+        'Accept': 'application/json',
+    }
 })
+
+axiosIns.interceptors.request.use(
+    config => {
+        const token = localStorage.getItem('token')
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config
+    }
+)
+
+axios.interceptors.response.use(
+    response => response,
+    error => {
+        const snackbarStore = useGlobalSnackbarStore()
+        if (error.response.status == 500)
+            {
+                snackbarStore.setValues({
+                    show: true,
+                    message: 'Server Error',
+                    color: 'error'
+                })
+            }
+        
+        if (error.response.status == 401)
+            {
+                snackbarStore.setValues({
+                    show: true,
+                    message: 'Unauthorized login, please login first.',
+                    color: 'error'
+                })
+            }
+    }
+)
 
 export default axiosIns
--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { useGlobalSnackbarStore } from '@/store/GlobalSnackbar'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -54,5 +55,23 @@ const router = createRouter({
   ],
   
 })
+
+router.beforeEach((to, from, next) => {
+  const snackbarStore = useGlobalSnackbarStore()
+  if (to.name !== 'login' && !localStorage.getItem('token')) {
+    snackbarStore.setValues({
+      show: true,
+      message: 'Unauthorized login, please login first.',
+      color: 'error'
+    })
+    next({ name: 'login' })
+  } else {
+     if (to.path == '/login'){
+      next({path: '/'})
+     } else {
+      next()
+     }
+  }
+}) 
 
 export default router
