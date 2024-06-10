cfba3fe6529e5bb33d46e3b21503c162a12331ad
sairlseb <cyrilseberiaga@gmail.com>
2024-06-10 08:21:09 +0800

    Remove Unnecessary Elements
    - Removed Trademarks, watermarks in Footer

--- resources/js/layouts/components/DefaultLayoutWithVerticalNav.vue
+++ resources/js/layouts/components/DefaultLayoutWithVerticalNav.vue
@@ -31,28 +31,7 @@ const upgradeBanner = computed(() => {
           <VIcon icon="bx-menu" />
         </IconBtn>
 
-        <!-- 👉 Search -->
-        <div
-          class="d-flex align-center cursor-pointer"
-          style="user-select: none;"
-        >
-          <!-- 👉 Search Trigger button -->
-          <IconBtn>
-            <VIcon icon="bx-search" />
-          </IconBtn>
-
-          <span class="d-none d-md-flex align-center text-disabled">
-            <span class="me-3">Search</span>
-            <span class="meta-key">&#8984;K</span>
-          </span>
-        </div>
-
         <VSpacer />
-
-        <IconBtn class="me-2">
-          <VIcon icon="bx-bell" />
-        </IconBtn>
-
         <NavbarThemeSwitcher class="me-2" />
 
         <UserProfile />
