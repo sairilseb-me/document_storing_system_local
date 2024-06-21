7c21cf08d83af6b066e166fc90b1678d35a1cdc0
sairlseb <cyrilseberiaga@gmail.com>
2024-06-21 10:19:50 +0800

    feature/create-error-404-page
    - created a 404 page route and page

--- app/Http/Middleware/CheckNasConnection.php
+++ app/Http/Middleware/CheckNasConnection.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use App\Services\PingNas;
-use Illuminate\Support\Facades\Auth;
+
 
 class CheckNasConnection
 {
---
+++ resources/js/views/pages/not-found.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="d-flex w-100 h-screen">
+    <v-row>
+      <v-col cols="12" class="d-flex justify-center align-center">
+        <v-card class="pa-10">
+          <v-card-title class="text-h5">404 Not Found</v-card-title>
+          <v-card-text>
+            <p>Sorry, the page you are looking for could not be found.</p>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="primary" to="/">Go to Home</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
