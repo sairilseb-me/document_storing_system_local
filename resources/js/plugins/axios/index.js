74e2f64c7d137d6969e3a81e73216273a8791c94
Sai Seberiaga <cyrilseberiaga@gmail.com>
2024-07-16 15:19:55 +0800

    update axios baseURL

--- resources/js/plugins/axios/index.js
+++ resources/js/plugins/axios/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useGlobalSnackbarStore } from '@/store/GlobalSnackbar'
 
 const axiosIns = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: `${import.meta.env.VITE_API_URL}/api`,
     headers: {
         'Accept': 'application/json',
     }
